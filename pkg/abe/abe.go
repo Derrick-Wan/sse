c5856b2a5826d534e73a9f1f716f4c343cababb6
Derrick Wan <scuderrickwan@gmail.com>
2023-09-05 11:51:50 +0800

    O.o
    Signed-off-by: Derrick Wan <scuderrickwan@gmail.com>

--- pkg/abe/abe.go
+++ pkg/abe/abe.go
@@ -39,7 +39,7 @@ const (
 
 var iskNum int = 5
 var itNum int = 5
-var sk_len int
+var skLen int
 
 type ABE interface {
 	Setup([]byte)
@@ -79,7 +79,7 @@ func (v *abe) GenerateInterMediateSecretKey() []byte {
 	var iskRet int
 	C.iie_abe_keygen_out(&v.pk, isk, C.int(iskNum), (*C.int)(unsafe.Pointer(&iskRet)))
 	fmt.Printf("iskRet %v\n", iskRet)
-	return v.unsafePointerToBytes(unsafe.Pointer(isk), iskRet)
+	return v.unsafePointerToBytes(unsafe.Pointer(isk), C.MIN_UNIT_LEN*(C.ISK_UNIT_LEN*(1+iskNum)+C.COUNT_MARK_LEN))
 }
 
 // online PKG generate user sk   attr="信息部,运维处,部长"
@@ -88,24 +88,24 @@ func (v *abe) GenerateUserSecretKey(isk1 []byte, isk2 []byte, attr []byte) []byt
 	fmt.Printf("secretKeyNum: %v \n", secretKeyNum)
 	secretKeyLen := C.SK_UNIT_LEN*(secretKeyNum+2) + 3 + cgoStrlen(attr) + cgoStrlen(v.dft)
 	secretKey := (*C.uchar)(C.malloc(C.ulong(C.MIN_UNIT_LEN * secretKeyLen)))
-	sk_len = secretKeyLen
+	skLen = secretKeyLen
 	defer C.free(unsafe.Pointer(secretKey))
 	var secretKeyRet int
 	C.iie_abe_keygen_pkg(&v.msk, (*C.uchar)(unsafe.Pointer(&attr[0])), (*C.uchar)(v.bytesToUnsafePointer(isk1)), (*C.uchar)(v.bytesToUnsafePointer(isk2)), secretKey, (*C.int)(unsafe.Pointer(&secretKeyRet)))
 	fmt.Printf("secretKeyRet %v\n", secretKeyRet)
-	return v.unsafePointerToBytes(unsafe.Pointer(secretKey), secretKeyRet)
+	return v.unsafePointerToBytes(unsafe.Pointer(secretKey), C.MIN_UNIT_LEN*secretKeyLen)
 }
 
 // PKG generate tk: Conversion Key rtk: Retrieval Key
 func (v *abe) GenerateTKAndRTK(sk []byte) ([]byte, []byte) {
-	tk := (*C.uchar)(C.malloc(C.ulong(int(C.ptr_size()) * sk_len)))
+	tk := (*C.uchar)(C.malloc(C.ulong(int(C.ptr_size()) * skLen)))
 	rtk := (*C.uchar)(C.malloc(C.ulong(C.ZP_BYTES_LEN)))
 	defer C.free(unsafe.Pointer(tk))
 	defer C.free(unsafe.Pointer(rtk))
 	var tkRet int
 	C.iie_abe_keygen_ran(&v.pk, (*C.uchar)(v.bytesToUnsafePointer(sk)), tk, rtk, (*C.int)(unsafe.Pointer(&tkRet)))
 	fmt.Printf("tkRet: %v\n", tkRet)
-	return v.unsafePointerToBytes(unsafe.Pointer(tk), sk_len), v.unsafePointerToBytes(unsafe.Pointer(rtk), sk_len)
+	return v.unsafePointerToBytes(unsafe.Pointer(tk), int(C.ptr_size())*skLen), v.unsafePointerToBytes(unsafe.Pointer(rtk), C.ZP_BYTES_LEN)
 }
 
 // user offline generate intermediate ciphertext
@@ -114,7 +114,35 @@ func (v *abe) GenerateIntermediateCiphertext() []byte {
 	defer C.free(unsafe.Pointer(it))
 	var itRet int
 	C.iie_abe_encrypt_out(&v.pk, it, C.int(itNum), (*C.int)(unsafe.Pointer(&itRet)))
-	return v.unsafePointerToBytes(unsafe.Pointer(it), itRet)
+	return v.unsafePointerToBytes(unsafe.Pointer(it), C.MIN_UNIT_LEN*(C.IT_UNIQUE_LEN+itNum*C.IT_UNIT_LEN+C.COUNT_MARK_LEN))
+}
+
+// user offline encrypt return intermediateCiphertext
+func (v *abe) EncryptOffline() []byte {
+	intermediateCiphertext := (*C.uchar)(C.malloc(C.ulong(C.MIN_UNIT_LEN * (C.IT_UNIQUE_LEN + itNum*C.IT_UNIT_LEN + C.COUNT_MARK_LEN))))
+	defer C.free(unsafe.Pointer(intermediateCiphertext))
+	var itRet int
+	C.iie_abe_encrypt_out(&v.pk, intermediateCiphertext, C.int(itNum), (*C.int)(unsafe.Pointer(&itRet)))
+	fmt.Printf("itRet: %v\n", itRet)
+	return v.unsafePointerToBytes(unsafe.Pointer(intermediateCiphertext), C.MIN_UNIT_LEN*(C.IT_UNIQUE_LEN+itNum*C.IT_UNIT_LEN+C.COUNT_MARK_LEN))
+}
+
+// user online encrypt
+func (v *abe) EncryptOnline(plainText []byte, policy []byte, it1 []byte, it2 []byte) []byte {
+	ctNum := int(C.get_policy_count((*C.char)(unsafe.Pointer(&policy[0]))))
+	fmt.Printf("ct_num: %v\n", ctNum)
+	ctLen := C.CT_UNIQUE_LEN + C.CT_UNIT_LEN*(ctNum+1) + 8 + cgoStrlen(policy) + cgoStrlen(v.dft) + C.COUNT_MARK_LEN*2 + cgoStrlen(plainText) + C.IV_LEN + C.TAG_LEN
+	ct := (*C.uchar)(C.malloc(C.ulong(C.MIN_UNIT_LEN * ctLen)))
+	defer C.free(unsafe.Pointer(ct))
+	var ctRetLen int
+	C.iie_abe_encrypt_user(&v.pk, (*C.uchar)(v.bytesToUnsafePointer(it1)), (*C.uchar)(v.bytesToUnsafePointer(it2)), ct, (*C.uchar)(unsafe.Pointer(&policy[0])), (*C.uchar)(unsafe.Pointer(&plainText[0])), (*C.int)(unsafe.Pointer(&ctRetLen)))
+	fmt.Printf("ctRetLen: %v\n", ctRetLen)
+	return v.unsafePointerToBytes(unsafe.Pointer(ct), C.MIN_UNIT_LEN*ctLen)
+}
+
+// user offline decrypt
+func (v *abe) DecryptOffline(ciphertext []byte, policy []byte, it1 []byte, it2 []byte) []byte {
+	return nil
 }
 
 func (v *abe) unsafePointerToBytes(ptr unsafe.Pointer, length int) []byte {
@@ -126,7 +154,6 @@ func (v *abe) unsafePointerToBytes(ptr unsafe.Pointer, length int) []byte {
 	return res
 }
 
-// TODO possible page fault
 func (v *abe) bytesToUnsafePointer(bytes []byte) unsafe.Pointer {
 	return unsafe.Pointer(&bytes[0])
 }
